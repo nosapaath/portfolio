c27a981cf71869e565f2f6aa94ac1917b1855972
Nosa Paath <adepaath@gmail.com>
2019-02-06 22:38:07 -0800

    changed layout

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(function() {
 
-  setTimeout(function () {
-    $(".title-type").html("<h1>I am a Web Developer</h1>")},3500);
+  if ( $(window).width() > 620) {      
+    setTimeout(function () {
+      $(".title-type").html("<h1>I am a Web Developer</h1>")},3500);
+  } 
+  else {
+    return;
+  }
+  
 
   $("#btn-down").on("click", function(event) {
     event.preventDefault();
@@ -11,16 +17,41 @@ $(function() {
 
   console.log('%c Hire Me!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
 
-  let winH = $(window).scrollY;  
-  if ($(window).width() < 620) {
-    if(winH > window.innerHeight){
-      $("#hlight").addClass("hlighter");
-    } else{
-      $("#hlight").removeClass("hlighter");
+  //menu
+  const $menuBtn = $('#menu-btn');
+  const $menu = $('.menu');
+  const $menuNav = $('.menu-nav');
+  const $menuBranding = $('.menu-branding');
+  const $navItems = $('.nav-item');
+
+  //initial state of menu
+  let $showMenu = false;
+
+  $menuBtn.click(function(){
+    console.log('work');
+    toggleMenu();
+  });
+
+  function toggleMenu() {
+    if (!$showMenu) {
+      $menuBtn.addClass('close');
+      $menu.addClass('show');
+      $menuNav.addClass('show');
+      $menuBranding.addClass('show');
+      $('.nav-item').addClass('show');
+  
+      // Set Menu State
+      $showMenu = true;
+    } else {
+      $menuBtn.removeClass('close');
+      $menu.removeClass('show');
+      $menuNav.removeClass('show');
+      $menuBranding.removeClass('show');
+      $('.menu-nav a:has(.nav-item)').removeClass('show');
+  
+      // Set Menu State
+      $showMenu = false;
     }
-  }
-  else {
-    return;
   }
 });
 
