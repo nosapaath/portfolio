3fc7bd2718fc7d43c0df5837899c00f9b59ed965
Nosa Paath <adepaath@gmail.com>
2019-02-07 20:43:47 -0800

    added new fonts, footer, branding

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 $(function() {
 
-  if ( $(window).width() > 620) {      
-    setTimeout(function () {
-      $(".title-type").html("<h1>I am a Web Developer</h1>")},3500);
-  } 
-  else {
-    return;
-  }
+  // if ( $(window).width() > 620) {      
+  //   setTimeout(function () {
+  //     $(".title-type").html("<h1>I am a Web Developer</h1>")},3500);
+  // } 
+  // else {
+  //   return;
+  // }
   
-
-  $("#btn-down").on("click", function(event) {
-    event.preventDefault();
-    const new_position = $("#scroll-to").offset();
-    $('html, body').animate({scrollTop : new_position.top }, 1000);
-  });
+  // <i onclick='' id="btn-down" class="fas fa-arrow-down fa-2x button" ></i>
+  // $("#btn-down").on("click", function(event) {
+  //   event.preventDefault();
+  //   const new_position = $("#scroll-to").offset();
+  //   $('html, body').sc({scrollTop : new_position.top }, 1000);
+  // });
 
   console.log('%c Hire Me!', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
 
